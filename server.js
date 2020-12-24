231041d3cd9d48d0772668912a741abe2c11a4ec
Mike Shelby <mikegshelby@gmail.com>
2020-12-24 16:18:01 -0600

    add routes to display all notes or specific notes based on provided id

--- server.js
+++ server.js
@@ -5,13 +5,31 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+// filter notes array based on a provided id
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+  }
+
+// Display main landing page
 app.get('/', (req, res) => {
     res.send('Main page');
   });
 
+// Display all notes
 app.get('/api/notes', (req, res) => {
     res.json(notes);
   });
+
+// Display note data based on a provided id
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
 
 
 app.listen(PORT, () => {
